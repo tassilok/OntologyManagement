dd97d2f4aaf44a01b21092a7bf3628d1ad2c09f8
Tassilo Koller <tassilok@gmx.de>
2013-06-11 07:00:35 +0200

    Go On

---
+++ ElasticSearchToXML/ElasticSearchToXML/clsXMLevel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ontolog_Module;
+using System.Xml;
+using System.IO;
+
+namespace ElasticSearchToXML
+{
+    class clsXMLevel
+    {
+        private clsGlobals objGlobals;
+        private clsDBLevel objDbLevel;
+        private XmlWriter objXmlWriter;
+
+        public void Initialize_XML(Stream objStream)
+        {
+            objXmlWriter = new XmlTextWriter(objStream,System.Text.Encoding.UTF8);
+        }
+
+
+        public void Finalize_XML()
+        {
+            objXmlWriter.Close();
+        }
+
+        public clsXMLevel(clsGlobals globals)
+        {
+            objGlobals = globals;
+            set_DBConnection();
+        }
+
+        private void set_DBConnection()
+        {
+            objDbLevel = new clsDBLevel(objGlobals);
+        }
+
+        clsOntologyItem get_Data_AttributeType(List<clsOntologyItem> oListAttType = null
+                                              ,Boolean boolTable = false
+                                              ,Boolean doCount = false)
+        {
+            clsOntologyItem objResult;
+
+            objResult =  objDbLevel.get_Data_AttributeType(oListAttType
+                                              , boolTable
+                                              , doCount);
+
+            
+
+            return objResult;
+        }
+        
+    }
+}
---
+++ ElasticSearchToXML/ElasticSearchToXML/frmMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ontolog_Module;
+
+namespace ElasticSearchToXML
+{
+    public partial class frmMain : Form
+    {
+        private readonly clsGlobals objGlobals;
+        private clsDBLevel objDbLevel;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            objGlobals = new clsGlobals();
+            set_DBConnection();
+        }
+
+        private void set_DBConnection()
+        {
+            objDbLevel = new clsDBLevel(objGlobals);
+        }
+    }
+}
