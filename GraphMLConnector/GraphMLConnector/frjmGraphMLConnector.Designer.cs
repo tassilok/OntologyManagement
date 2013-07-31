74f88a07f64f0cef66014c6c98ccb0c1f143a80a
Tassilo Koller <tassilok@gmx.de>
2013-07-31 17:16:44 +0200

    Create UML-Diagrams

---
+++ GraphMLConnector/GraphMLConnector/frjmGraphMLConnector.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace GraphMLConnector
+{
+    partial class frmGraphMLConnector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmGraphMLConnector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 487);
+            this.Name = "frmGraphMLConnector";
+            this.Text = "Graph-ML Connector";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
