a4e46485e819091578a82e172807261758e051e1
Tassilo Koller <tassilok@gmx.de>
2013-11-27 00:10:56 +0100

    Go On

--- MSBuild/MSBuild.cmd
+++ MSBuild/MSBuild.cmd
@@ -23,3 +23,4 @@ C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe %USERPROFILE%\Docume
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe %USERPROFILE%\Documents\GitHub\OntologyManagement\OutlookConnector-Module\OutlookConnector-Module.sln
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe %USERPROFILE%\Documents\GitHub\OntologyManagement\TimeManagement-Module\TimeManagement-Module.sln
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe %USERPROFILE%\Documents\GitHub\OntologyManagement\SemToOntMigration\SemToOntMigration.sln
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe %USERPROFILE%\Documents\GitHub\OntologyManagement\TextParser\TextParser.sln
