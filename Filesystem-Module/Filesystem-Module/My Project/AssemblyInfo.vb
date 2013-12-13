f4de085dcc7fba4fea61ed19582fbe71214ac6d4
Tassilo Koller <tassilok@gmx.de>
2013-12-13 17:41:23 +0100

    Go On

--- Filesystem-Module/Filesystem-Module/My Project/AssemblyInfo.vb
+++ Filesystem-Module/Filesystem-Module/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.2.0.9")> 
-<Assembly: AssemblyFileVersion("0.2.0.9")> 
+<Assembly: AssemblyVersion("0.2.0.10")> 
+<Assembly: AssemblyFileVersion("0.2.0.10")> 
