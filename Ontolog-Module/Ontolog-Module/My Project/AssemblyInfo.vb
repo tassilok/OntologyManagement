0f17b442fad3677befcac2c4e6437d63b61c9fc6
Tassilo Koller <tassilok@gmx.de>
2014-01-10 00:59:48 +0100

    Go on

--- Ontolog-Module/Ontolog-Module/My Project/AssemblyInfo.vb
+++ Ontolog-Module/Ontolog-Module/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.3.0.49")> 
-<Assembly: AssemblyFileVersion("0.3.0.49")> 
+<Assembly: AssemblyVersion("0.3.0.50")> 
+<Assembly: AssemblyFileVersion("0.3.0.50")> 
