c1e062e7536b4d653b593f24974cdd028ab35817
Tassilo Koller <tassilok@gmx.de>
2014-06-04 03:58:28 +0200

    Go On

--- Filesystem-Module/Filesystem-Module/clsBaseConfig.vb
+++ Filesystem-Module/Filesystem-Module/clsBaseConfig.vb
@@ -1,5 +1,6 @@
 ﻿Imports Ontology_Module
 Imports OntologyClasses.BaseClasses
+Imports System.IO
 
 Public Class clsBaseConfig
     Private objLocalConfig As clsLocalConfig
@@ -8,6 +9,7 @@ Public Class clsBaseConfig
 
     Private objDBLevel_Watch As clsDBLevel
     Private objDBLevel_Sources As clsDBLevel
+    Private objDBLevel_Server As clsDBLevel
 
     Private objTransaction As clsTransaction
     Private objRelationConfig As clsRelationConfig
@@ -77,6 +79,8 @@ Public Class clsBaseConfig
                                 Else
                                     boolPathGet = False
                                 End If
+                            Else
+                                boolPathGet = False
                             End If
                         Else
                             strPath = Nothing
@@ -136,6 +140,8 @@ Public Class clsBaseConfig
                             If OItem_Path_Sources.Name.ToLower() = OItem_Path_Watch.Name.ToLower() Then
                                 getPath = True
                             End If
+                        Else
+                            getPath = False
                         End If
                     End If
 
@@ -206,6 +212,110 @@ Public Class clsBaseConfig
 
         End If
 
+        If objOItem_Result.GUID = objLocalConfig.Globals.LState_Success.GUID Then
+            objOItem_Result = TestServer()
+        End If
+
+        Return objOItem_Result
+    End Function
+
+    Private Function TestServer() As clsOntologyItem
+        Dim objOItem_Result = objLocalConfig.Globals.LState_Success.Clone()
+
+        Dim objSearch_State = New List(Of clsObjectRel) From {New clsObjectRel With {.ID_Object = objLocalConfig.Globals.OItem_Server.GUID,
+                                                                                      .ID_RelationType = objLocalConfig.OItem_RelationType_isInState.GUID,
+                                                                                      .ID_Parent_Other = objLocalConfig.OItem_Type_Server_State.GUID}}
+
+        objOItem_Result = objDBLevel_Server.get_Data_ObjectRel(objSearch_State, boolIDs:=False)
+
+        If objOItem_Result.GUID = objLocalConfig.Globals.LState_Success.GUID Then
+            If Not objDBLevel_Server.OList_ObjectRel.Any() Then
+                objTransaction.ClearItems()
+                Dim objORel_Server_To_State = objRelationConfig.Rel_ObjectRelation(objLocalConfig.Globals.OItem_Server,
+                                                                                   objLocalConfig.OItem_Token_Active_Server_State,
+                                                                                   objLocalConfig.OItem_RelationType_isInState)
+                objOItem_Result = objTransaction.do_Transaction(objORel_Server_To_State, True)
+
+                
+
+            End If
+
+            If objOItem_Result.GUID = objLocalConfig.Globals.LState_Success.GUID Then
+                Dim objSearch_Type = New List(Of clsObjectRel) From {New clsObjectRel With {.ID_Object = objLocalConfig.Globals.OItem_Server.GUID,
+                                                                                            .ID_RelationType = objLocalConfig.OItem_RelationType_is_of_Type.GUID,
+                                                                                            .ID_Parent_Other = objLocalConfig.OItem_Type_Server_Type.GUID}}
+
+                objOItem_Result = objDBLevel_Server.get_Data_ObjectRel(objSearch_Type, boolIDs:=False)
+
+                If objOItem_Result.GUID = objLocalConfig.Globals.LState_Success.GUID Then
+                    If Not objDBLevel_Server.OList_ObjectRel.Any() Then
+                        Dim objORel_Server_To_Type = objRelationConfig.Rel_ObjectRelation(objLocalConfig.Globals.OItem_Server,
+                                                                                          objLocalConfig.OItem_Token_Fileserver_Server_Type,
+                                                                                          objLocalConfig.OItem_RelationType_is_of_Type)
+
+                        objOItem_Result = objTransaction.do_Transaction(objORel_Server_To_Type, True)
+
+                        
+                    End If
+                End If
+            End If
+        End If
+        If objOItem_Result.GUID = objLocalConfig.Globals.LState_Success.GUID Then
+            objOItem_Result = TestDrives()
+
+        End If
+        Return objOItem_Result
+    End Function
+
+    Private Function TestDrives() As clsOntologyItem
+        Dim objOItem_Result = objLocalConfig.Globals.LState_Success.Clone()
+
+        Dim allDrives = DriveInfo.GetDrives()
+        Dim objSearch_Drive = New List(Of clsObjectRel) From {New clsObjectRel With {.ID_Other = objLocalConfig.Globals.OItem_Server.GUID,
+                                                                                              .ID_RelationType = objLocalConfig.OItem_RelationType_isSubordinated.GUID,
+                                                                                              .ID_Parent_Object = objLocalConfig.OItem_Type_Drive.GUID}}
+
+        objOItem_Result = objDBLevel_Server.get_Data_ObjectRel(objSearch_Drive, boolIDs:=False)
+        If objOItem_Result.GUID = objLocalConfig.Globals.LState_Success.GUID Then
+            For Each drive In allDrives
+                If drive.DriveType = DriveType.CDRom Or drive.DriveType = DriveType.Fixed Then
+                    Dim strDrive = drive.Name.Replace("\", "")
+
+
+                    If objOItem_Result.GUID = objLocalConfig.Globals.LState_Success.GUID Then
+                        If Not objDBLevel_Server.OList_ObjectRel.Where(Function(driveDb) driveDb.Name_Object.ToLower() = strDrive.ToLower()).Any() Then
+                            objTransaction.ClearItems()
+                            Dim objOItem_Drive = New clsOntologyItem With {.GUID = objLocalConfig.Globals.NewGUID,
+                                                                           .Name = strDrive,
+                                                                           .GUID_Parent = objLocalConfig.OItem_Type_Drive.GUID,
+                                                                           .Type = objLocalConfig.Globals.Type_Object}
+
+                            objOItem_Result = objTransaction.do_Transaction(objOItem_Drive)
+
+                            If objOItem_Result.GUID = objLocalConfig.Globals.LState_Success.GUID Then
+                                Dim objRel_Drive_To_Server = objRelationConfig.Rel_ObjectRelation(objOItem_Drive,
+                                                                                                  objLocalConfig.Globals.OItem_Server,
+                                                                                                  objLocalConfig.OItem_RelationType_isSubordinated)
+
+                                objOItem_Result = objTransaction.do_Transaction(objRel_Drive_To_Server)
+
+                                If objOItem_Result.GUID = objLocalConfig.Globals.LState_Error.GUID Then
+                                    objTransaction.rollback()
+
+                                    Exit For
+                                End If
+                            Else
+                                Exit For
+                            End If
+                        End If
+                    Else
+                        Exit For
+                    End If
+                End If
+            Next
+        End If
+        
+
         Return objOItem_Result
     End Function
 
@@ -218,6 +328,7 @@ Public Class clsBaseConfig
     Private Sub Initialize()
         objDBLevel_Watch = New clsDBLevel(objLocalConfig.Globals)
         objDBLevel_Sources = New clsDBLevel(objLocalConfig.Globals)
+        objDBLevel_Server = New clsDBLevel(objLocalConfig.Globals)
 
         objTransaction = New clsTransaction(objLocalConfig.Globals)
         objRelationConfig = New clsRelationConfig(objLocalConfig.Globals)
