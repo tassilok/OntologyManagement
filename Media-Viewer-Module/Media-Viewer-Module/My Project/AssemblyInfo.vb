01116b46cf638e348402071f3826814e3118eee2
Tassilo Koller <tassilok@gmx.de>
2014-08-04 19:03:09 +0200

    Go On

--- Media-Viewer-Module/Media-Viewer-Module/My Project/AssemblyInfo.vb
+++ Media-Viewer-Module/Media-Viewer-Module/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.1.79")> 
-<Assembly: AssemblyFileVersion("0.0.1.79")> 
+<Assembly: AssemblyVersion("0.0.1.80")> 
+<Assembly: AssemblyFileVersion("0.0.1.80")> 
