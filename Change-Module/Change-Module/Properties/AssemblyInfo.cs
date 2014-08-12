534409a90c17a89f0761754bcbce52b3df4d6fa1
Tassilo Koller <tassilok@gmx.de>
2014-08-12 17:55:54 +0200

    Go On

--- Change-Module/Change-Module/Properties/AssemblyInfo.cs
+++ Change-Module/Change-Module/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.62")]
-[assembly: AssemblyFileVersion("0.2.0.62")]
+[assembly: AssemblyVersion("0.2.0.65")]
+[assembly: AssemblyFileVersion("0.2.0.65")]
--- Ontolog-Module/Ontolog-Module/My Project/AssemblyInfo.vb
+++ Ontolog-Module/Ontolog-Module/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.3.0.109")> 
-<Assembly: AssemblyFileVersion("0.3.0.109")> 
+<Assembly: AssemblyVersion("0.3.0.110")> 
+<Assembly: AssemblyFileVersion("0.3.0.110")> 
