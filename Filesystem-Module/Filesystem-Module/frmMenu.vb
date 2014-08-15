d5b377918a6d02d9584c95818cbc504fa8c660b2
Tassilo Koller <tassilok@gmx.de>
2014-08-15 23:41:59 +0200

    Go On

---
+++ Filesystem-Module/Filesystem-Module/frmMenu.vb
@@ -0,0 +1,12 @@
+﻿Public Class frmMenu
+    Private objLocalConfig As clsLocalConfig
+
+    Public Sub New(LocalConfig As clsLocalConfig)
+
+        ' Dieser Aufruf ist für den Designer erforderlich.
+        InitializeComponent()
+
+        ' Fügen Sie Initialisierungen nach dem InitializeComponent()-Aufruf hinzu.
+
+    End Sub
+End Class
