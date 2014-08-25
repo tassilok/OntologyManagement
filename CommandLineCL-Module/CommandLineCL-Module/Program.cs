449d897c5eeabc7915a26ff2e993b7a5afc0a095
Tassilo Koller <tassilo.koller@explido.de>
2014-08-25 15:25:38 +0200

    Go On

---
+++ CommandLineCL-Module/CommandLineCL-Module/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CommandLineRun_Module;
+
+namespace CommandLineCL_Module
+{
+    class Program
+    {
+        private static frmCommandLineRun objFrmCommandLineRun;
+
+        static void Main(string[] args)
+        {
+            var objShellOutput = new clsShellOutput();
+
+            objFrmCommandLineRun = new frmCommandLineRun(args, objShellOutput);
+            if (string.IsNullOrEmpty(objShellOutput.ErrorText))
+            {
+                Console.WriteLine(objShellOutput.OutputText);
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine(objShellOutput.OutputText);
+                Environment.Exit(-1);
+            }
+            
+        }
+    }
+}
---
+++ CommandLineRun-Module/CommandLineRun-Module/clsShellOutput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineRun_Module
+{
+    public class clsShellOutput
+    {
+        public bool ErrorOccured { get; set; }
+        public string ErrorText { get; set; }
+        public string OutputText { get; set; }
+    }
+}
