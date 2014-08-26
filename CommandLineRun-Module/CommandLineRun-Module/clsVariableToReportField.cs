d46ea805a8e55c4bca9863dcbad496712addcdab
Tassilo Koller <tassilo.koller@explido.de>
2014-08-26 13:07:05 +0200

    Go On

---
+++ CommandLineRun-Module/CommandLineRun-Module/clsVariableToReportField.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineRun_Module
+{
+    public class clsVariableToReportField
+    {
+        public string ID_Variable { get; set; }
+        public string Name_Variable { get; set; }
+        public string ID_ReportField { get; set; }
+        public string Name_ReportField { get; set; }
+        public string ID_CommandLineRun { get; set; }
+        public string Name_CommandLineRun { get; set; }
+    }
+}
