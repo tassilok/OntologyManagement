1616ace2c5ceb7e1d67afefa26bb9619d3c7245a
Tassilo Koller <tassilok@gmx.de>
2014-10-26 20:17:34 +0100

    guidCommit:dea58dbc4dab4729914578de54214661

--- Bill-Module/Bill-Module/My Project/AssemblyInfo.vb
+++ Bill-Module/Bill-Module/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.0.84")> 
-<Assembly: AssemblyFileVersion("0.0.0.84")> 
+<Assembly: AssemblyVersion("0.0.0.85")> 
+<Assembly: AssemblyFileVersion("0.0.0.85")> 
