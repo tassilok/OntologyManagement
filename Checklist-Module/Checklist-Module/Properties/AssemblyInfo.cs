37f0447ccfe298539c49685a97280befc59746ef
Tassilo Koller <tassilok@gmx.de>
2014-10-26 20:19:06 +0100

    guidCommit:dea58dbc4dab4729914578de54214661

--- Checklist-Module/Checklist-Module/Properties/AssemblyInfo.cs
+++ Checklist-Module/Checklist-Module/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.95")]
-[assembly: AssemblyFileVersion("0.0.0.95")]
+[assembly: AssemblyVersion("0.0.0.96")]
+[assembly: AssemblyFileVersion("0.0.0.96")]
