19968a431e3171f2b0502542de9b6982da97bc97
Tassilo Koller <tassilok@gmx.de>
2014-12-05 14:35:30 +0100

    guidCommit:0ee71c0ae3b442c9926752c9dfe6477f

--- Report-Module/Report-Module/My Project/AssemblyInfo.vb
+++ Report-Module/Report-Module/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.1.102")> 
-<Assembly: AssemblyFileVersion("0.0.1.102")> 
+<Assembly: AssemblyVersion("0.0.1.103")> 
+<Assembly: AssemblyFileVersion("0.0.1.103")> 
