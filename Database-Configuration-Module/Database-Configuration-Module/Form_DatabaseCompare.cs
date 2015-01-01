e2d45d0a4ae594b3d0408132e0fef834662a022f
Tassilo Koller <tassilok@gmx.de>
2015-01-01 11:15:39 +0100

    guidCommit:8dca2d9322074cd8a0d91f6534007262

---
+++ Database-Configuration-Module/Database-Configuration-Module/Form_DatabaseCompare.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatabaseConfigurationModule
+{
+    public partial class Form_DatabaseCompare : Form
+    {
+        public Form_DatabaseCompare()
+        {
+            InitializeComponent();
+        }
+    }
+}
