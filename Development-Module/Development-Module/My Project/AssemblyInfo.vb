00944f22a4c76e0c635b4147cf8f17e84edb72f5
Tassilo Koller <tassilok@gmx.de>
2015-01-06 10:44:49 +0100

    guidCommit:b1053a0aecdc4c95a272dca47479d312

--- Development-Module/Development-Module/My Project/AssemblyInfo.vb
+++ Development-Module/Development-Module/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.2.0.123")> 
-<Assembly: AssemblyFileVersion("0.2.0.123")> 
+<Assembly: AssemblyVersion("0.2.0.124")> 
+<Assembly: AssemblyFileVersion("0.2.0.124")> 
