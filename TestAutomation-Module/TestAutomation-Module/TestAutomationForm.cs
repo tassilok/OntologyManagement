96e2b50a93d826f38853f8284515af03225a994a
Tassilo Koller <tassilok@gmx.de>
2015-04-06 08:06:11 +0200

    guidCommit:b896272d8a9840478fcbcf0e5e69304e

---
+++ TestAutomation-Module/TestAutomation-Module/TestAutomationForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestAutomation_Module
+{
+    public partial class TestAutomationForm : Form
+    {
+        public TestAutomationForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
